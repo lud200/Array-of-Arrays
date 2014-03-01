c441c6a4ead5a7b0b16cb98c87ad7feb3ee7ce5e
dludaya <dludaya.cs@gmail.com>
2014-03-01 03:24:16 -0500

    Word reverse

--- Array of Arrays/src/AofA/AofA.java
+++ Array of Arrays/src/AofA/AofA.java
@@ -1,5 +1,5 @@
 package AofA;
-
+import java.util.*;
 public class AofA {
 	public static void main(String[] args){
 		int[] a={2, 3, 1, 0};
@@ -8,6 +8,15 @@ public class AofA {
 			int pos=a[a[i]];
 			b[i]=pos;
 			System.out.println(a[i]);
-		}		
+		}
+			//word reversing
+			String s="Iam good girl";
+			String s1[]=s.split(" ");
+			StringBuilder sb=new StringBuilder();
+			for(int i=s1.length-1; i>=0; i--){
+				sb=sb.append(s1[i]+" ");		
+			}
+			String s2=sb.toString();
+			System.out.println(s2);				
 	}
 }
